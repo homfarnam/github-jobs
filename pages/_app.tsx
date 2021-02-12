ff513158a61ef5057cd12ce6593c84f0eadcb74b
homfarnam <homfarnam@gmail.com>
2021-02-12 10:54:27 +0330

    Use query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,32 @@
 import "tailwindcss/tailwind.css"
 import "../styles/index.scss"
- 
 
 import Head from "next/head"
+import { QueryClient, QueryClientProvider } from "react-query"
 
 function MyApp({ Component, pageProps }: any) {
+  const queryClient = new QueryClient()
+
   return (
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link
-            rel="preload"
-            href="/fonts/KumbhSans-Light.ttf"
-            as="font"
-            crossOrigin=""
-          />
-          <link
-            rel="preload"
-            href="/fonts/KumbhSans-Regular.ttf"
-            as="font"
-            crossOrigin=""
-          />
+          rel="preload"
+          href="/fonts/KumbhSans-Light.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/KumbhSans-Regular.ttf"
+          as="font"
+          crossOrigin=""
+        />
       </Head>
-
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </>
   )
 }
