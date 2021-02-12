b05ab04c56f4cdf46e4314c469ed232d8631b0c5
homfarnam <homfarnam@gmail.com>
2021-02-12 10:55:14 +0330

    Change layout

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -1,18 +1,37 @@
 import Head from "next/head"
+import { Dispatch, SetStateAction } from "react"
 
 import NavBar from "../components/NavBar/NavBar"
 
 interface LayoutProps {
   title: string
+  setMyLocation: Dispatch<SetStateAction<string>>
+  setDescription: Dispatch<SetStateAction<string>>
+  MyLocation: string
+  Description: string
+  // setFullTime: Dispatch<SetStateAction<string>>
 }
 
-const Layout: React.FC<LayoutProps> = ({ title, children }) => {
+const Layout: React.FC<LayoutProps> = ({
+  title,
+  children,
+  setMyLocation,
+  MyLocation,
+  setDescription,
+  Description,
+  // setFullTime,
+}) => {
   return (
     <>
       <Head>
         <title>{title}</title>
       </Head>
-      <NavBar />
+      <NavBar
+        setMyLocation={setMyLocation}
+        MyLocation={MyLocation}
+        setDescription={setDescription}
+        Description={Description}
+      />
       <div>{children}</div>
     </>
   )
